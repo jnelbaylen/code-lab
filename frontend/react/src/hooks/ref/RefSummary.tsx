daf53333a7f27dc1c4016beba5ee1112286d4916
jnelbaylen <jnelbaylen@gmail.com>
2025-12-27 23:53:59 +0800

    chore: import focusing

--- frontend/react/src/hooks/ref/RefSummary.tsx
+++ frontend/react/src/hooks/ref/RefSummary.tsx
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 
 import { RefCounter } from "./RefCounter";
 import { RefTimer } from "./RefTimer";
+import { RefFocusing } from "./RefFocusing";
 
 export function RefSummary(){
    return(
      <Fragment>
         <RefCounter />  
         <RefTimer /> 
+        <RefFocusing />
     </Fragment>
    )
 }
