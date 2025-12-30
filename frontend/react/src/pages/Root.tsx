b54562b02029d73b73990b470847e506ced2f798
jnelbaylen <jnelbaylen@gmail.com>
2025-12-31 00:40:34 +0800

    chore: import state and effect

--- frontend/react/src/pages/Root.tsx
+++ frontend/react/src/pages/Root.tsx
@@ -1,11 +1,15 @@
 import { Fragment } from 'react';
 
 import { RefSummary } from '@hooks/ref/RefSummary';
+import { StateSummary } from '@hooks/state/StateSummary';
+import { EffectSummary } from '@hooks/effect/EffectSummary';
 
 export function Root(){
     return(
         <Fragment>
             <RefSummary />
+            <StateSummary />
+            <EffectSummary />
         </Fragment>
     );
 }
