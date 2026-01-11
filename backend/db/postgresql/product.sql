bc367c04477bd0eab9b1ecbbb767b92a944499b2
jnelbaylen <jnelbaylen@gmail.com>
2026-01-11 12:33:19 +0800

    feat: add postgresql product

---
+++ backend/db/postgresql/product.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE product (
+-- 	product_name VARCHAR(200),
+-- 	price INTEGER
+-- );
+
+-- INSERT INTO product ( product_name, price )
+-- VALUES ( 'Dress', 100 ); 
+
+-- SELECT * 
+-- fFROM product;
+
+SELECT * 
+FROM product;
+
+SELECT product_name, price
+FROM product;
