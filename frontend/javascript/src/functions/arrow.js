66ad31a1e5261f2e73f58cce59d3a69c92716ff2
jnelbaylen <jnelbaylen@gmail.com>
2026-01-19 14:02:25 +0800

    feat: restructure javascript

--- frontend/javascript/src/functions/arrow.js
+++
@@ -1,6 +0,0 @@
-const message = () => {
-    let success = "Embrace challenges as opportunities, maintain a relentless focus on your goals, and never underestimate the power of perseverance.";
-    console.log(success);
-}
-
-message();
--- frontend/javascript/src/methods/map.js
+++
@@ -1,4 +0,0 @@
-const arrNumbers = [1, 3, 6, 12];
-const doubled = arrNumbers.map( x => x * 2);
-
-console.log( doubled );
