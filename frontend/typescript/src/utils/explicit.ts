8b2d1673caa9938d10ecdb25951e8983d58612c0
jnelbaylen <jnelbaylen@gmail.com>
2026-01-25 01:17:33 +0800

    feat: add explicit

---
+++ frontend/typescript/src/utils/explicit.ts
@@ -0,0 +1,21 @@
+// EXPLICIT
+const greeting: string = "Hello, World"
+const userCount: number = 10
+const isLoading: boolean = false
+
+const scores: number[] = [90, 99, 93]
+
+const sentence: string = `
+    Hi, this is my first ${ greeting }.
+    There are ${ userCount } users.
+    Loading status: ${ isLoading }.
+    My scores are ${ scores }.
+`
+
+console.log(sentence)
+
+function greet(fullName: string): string {
+    return `Hello, ${ fullName }`   
+}
+
+console.log(greet("John Doe"))
