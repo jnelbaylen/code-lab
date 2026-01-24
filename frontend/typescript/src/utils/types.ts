3b54ade5ae029eb297358b04a36c7f283c42ac8d
jnelbaylen <jnelbaylen@gmail.com>
2026-01-25 01:17:10 +0800

    feat: add types

---
+++ frontend/typescript/src/utils/types.ts
@@ -0,0 +1,32 @@
+// BOOLEAN 
+const isActive = true
+const hasPermission = true
+const canEdit = false
+const shouldUpdate = false
+
+console.log(isActive)
+console.log(hasPermission)
+console.log(canEdit)
+console.log(shouldUpdate)
+console.log("")
+
+// NUMBER
+const decimal = 6
+const hex = 0xf00d
+const binary = 0b1010
+const octal = 0o744
+const float = 3.14
+
+console.log(decimal)
+console.log(hex)
+console.log(binary)
+console.log(octal)
+console.log(float)
+console.log("")
+
+// STRING
+const color = "blue"
+const fullName = "John Doe"
+
+console.log(color)
+console.log(fullName)
